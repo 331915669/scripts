093b8965a846d0f1819bff2200eb8e2504b45e34
331915669 <331915669@qq.com>
2018-11-05 14:09:02 +0800

    修复中循环中变量取值问题

--- changePasswd.sh
+++ changePasswd.sh
@@ -5,10 +5,10 @@ for u in ${USER[*]}
 do
 PASSWORD="`</dev/urandom tr -dc '12345qwertQWERTasdfgASDFGzxcvbZXCVB' | head -c16|awk -F "" '{sub(//,"_",$6); print}' |sed 's/ //g'`"
 #echo "Changing the passwor of $USER "
-echo $PASSWORD |passwd $USER --stdin
+echo $PASSWORD |passwd $u --stdin
 if [ $? == 0 ]
 then
-    echo "New password for $USER:$PASSWORD"
+    echo "New password for $u:$PASSWORD"
 else
     echo "Fail to change the password!"
 fi
